6c7721eecaf82a75039e09dd769f4a621e8ea31f
Dmitry Pyatin <dmitry.pyatin@artezio.com>
2020-02-03 17:09:45 +0300

    Moved transaction settings to general config class

--- recovery-server/src/main/java/com/artezio/recovery/server/config/TransactionSupportConfig.java
+++ recovery-server/src/main/java/com/artezio/recovery/server/config/TransactionSupportConfig.java
@@ -1,22 +1,20 @@
 package com.artezio.recovery.server.config;
 
-import org.apache.camel.component.jms.JmsComponent;
 import org.apache.camel.spring.spi.SpringTransactionPolicy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jms.connection.JmsTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 
-import javax.jms.ConnectionFactory;
-
 @Configuration
-public class JMSConfig {
+public class TransactionSupportConfig {
 
-    @Bean(name = "PROPAGATION_REQUIRED")
+    public static final String PROPAGATIONTYPE_PROPAGATION_REQUIRED = "PROPAGATION_REQUIRED";
+
+    @Bean(name = PROPAGATIONTYPE_PROPAGATION_REQUIRED)
     public SpringTransactionPolicy propagationRequired(PlatformTransactionManager jtaTransactionManager){
         SpringTransactionPolicy propagationRequired = new SpringTransactionPolicy();
         propagationRequired.setTransactionManager(jtaTransactionManager);
-        propagationRequired.setPropagationBehaviorName("PROPAGATION_REQUIRED");
+        propagationRequired.setPropagationBehaviorName(PROPAGATIONTYPE_PROPAGATION_REQUIRED);
         return propagationRequired;
     }
 
